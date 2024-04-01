136bd41aafc77d2a56e89d94e90b189cee44222c
Nolan Darilek <nolan@thewordnerd.info>
2024-04-01 15:58:29 -0500

    Finish up followup support.

--- __init__.py
+++ __init__.py
@@ -17,6 +17,10 @@ for followupId in image.followups:
     followup = client.collection("followups").get_one(followupId)
     print(followup.text)
 
-followup = client.collection("followups").create(
+followups = client.collection("followups")
+followup = followups.create(
     {"text": "What types of trees are in the image?"})
+print("Sending followup question")
 image = images.update(image.id, {"followups+": followup.id})
+followup = followups.get_one(image.followups[-1])
+print(followup.text)
