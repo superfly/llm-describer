c6553cc88c0b3b6f77491e88eaf3f657b9075c20
Nolan Darilek <nolan@thewordnerd.info>
2024-04-02 11:19:53 -0500

    Prompt the user for followup questions.

--- __init__.py
+++ __init__.py
@@ -18,9 +18,13 @@ for followupId in image.followups:
     print(followup.text)
 
 followups = client.collection("followups")
-followup = followups.create(
-    {"text": "What types of trees are in the image?"})
-print("Sending followup question")
-image = images.update(image.id, {"followups+": followup.id})
-followup = followups.get_one(image.followups[-1])
-print(followup.text)
+
+while True:
+    question = input("Enter your followup question, or 'quit' to quit: ")
+    if question.lower() == "quit":
+        break
+    followup = followups.create({"text": question})
+    print("Sending followup question")
+    image = images.update(image.id, {"followups+": followup.id})
+    followup = followups.get_one(image.followups[-1])
+    print(followup.text)
